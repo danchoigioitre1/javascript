ea0ab124fe96afb23d78af1efd9ccd93a50f012f
danhchoigioitre1 <vietanhgalileo@gmail.com>
2022-04-03 17:41:18 +0700

    lam case

--- CASE STUDY/main-snake2.js
+++ CASE STUDY/main-snake2.js
@@ -9,74 +9,97 @@ const SNAKE_COLOR = 'white';
 const UNIT = 20;
 
 class vector {
-    x; y;
+
     constructor(x, y) {
         this.x = x;
         this.y = y;
     }
 }
 
-this.speed = new vector(-1, 0)
+
 
 class SNAKE {
 
-    body; speed;
-    constructor() {
+
+ constructor() {
         this.body = [
-            new vector(UNIT * 3, UNIT * 3),
-            new vector(UNIT * 4, UNIT * 3),
-            new vector(UNIT * 5, UNIT * 3)
-        ]
+            new vector(UNIT * 4, UNIT * 4),
+            new vector(UNIT * 5, UNIT * 4),
+            new vector(UNIT * 6, UNIT * 4),
+            new vector(UNIT * 7, UNIT * 4)
+        ];
 
         this.speed = new vector(-1, 0)
     }
 
     draw() {
-        ctx.beginPath();
+
         ctx.fillStyle = 'blue';
         ctx.fillRect(this.body[0].x, this.body[0].y, UNIT, UNIT)
-        ctx.closePath();
 
-        ctx.beginPath();
         ctx.fillStyle = SNAKE_COLOR;
-        for (var i = 1; i < this.body.length; i++) {
+        for (let i = 1; i < this.body.length; i++) {
             ctx.fillRect(this.body[i].x, this.body[i].y, UNIT, UNIT)
-
-
         }
-        ctx.closePath();
+
     }
 
-    clear() {
-        ctx.fillStyle = BACK_GROUND;
-        ctx.fillRect(this.body[0].x, this.body[0].y, UNIT, UNIT);
-
-        ctx.fillStyle = SNAKE_COLOR;
-
-        for (var i = 1; i < this.body.length; i++) {
-            ctx.fillRect(this.body[i].x, this.body[i].y, UNIT, UNIT);
-
-        }
-    }
-
+  
 
     move() {
-        
-        for (i = this.body.length - 1; i >= 1; i--) {
+
+     
+        for (i = 1; i < this.body.length; i++) {
             this.body[i].x = this.body[i - 1].x
             this.body[i].y = this.body[i - 1].y
-
-
-
         }
 
         this.body[0].x += this.speed.x * UNIT;
         this.body[0].y += this.speed.y * UNIT;
 
-        this.draw()
+
+        this.draw();
     }
+
+
+
+
+
+
 }
+
+
+window.addEventListener('keydown', (evt) => {
+    console.log(evt);
+    if (evt.key == "ArrowDown") {
+        player.speed.x = 0;
+        player.speed.y = 1;
+
+    }
+
+    if (evt.key == "ArrowUp") {
+        player.speed.x = 0;
+        player.speed.y = -1;
+
+    }
+
+    if (evt.key == "ArrowRight") {
+        player.speed.x = 1;
+        player.speed.y = 0;
+
+    }
+
+    if (evt.key == "ArrowLeft") {
+        player.speed.x = -1;
+        player.speed.y = 0;
+
+    }
+
+
+}
+)
 
 let player = new SNAKE()
 player.draw();
-setInterval(player.move, 1000);
+setInterval(player.move, 500);
+
